aa422e97d849cdb8a73bfac328efeb3ce5e44a98
miocrime <106968863+miocrime@users.noreply.github.com>
2022-06-15 23:10:07 +0300

    Update start.n.sh

--- root-scripts/start.n.sh
+++ root-scripts/start.n.sh
@@ -1,20 +1,18 @@
 #!/bin/bash
-cd "$(dirname "$0")" || exit; [ "$EUID" = "0" ] && exit
-export HOME="$PWD/files/data"; export XDG_DATA_HOME="$PWD/files/data/.local"; export XDG_CONFIG_HOME="$PWD/files/data/.config"; mkdir -p {"$HOME","$XDG_CONFIG_HOME","$XDG_DATA_HOME"}
+cd "$(dirname "$0")" || exit; [ "$EUID" = "0" ] && exit; R="$PWD"; F="$PWD/files"; export HOME="$F/data"; export XDG_DATA_HOME="$F/data/.local"; export XDG_CONFIG_HOME="$F/data/.config"; mkdir -p {"$HOME","$XDG_CONFIG_HOME","$XDG_DATA_HOME"}
 
-ROOTDIR="$PWD"; BINDIR="$PWD/files/groot"; BIN="game.bin"; echo "$BIN" >"$PWD/files/binval.txt"; CMD=(./"$BIN")
+BINDIR="$F/groot"; BIN="game.bin"; CMD=(./"$BIN")
 
 # gamescope/FSR
-: ${GAMESCOPE:=$(command -v gamescope)}; RRES=$(command -v rres); FSR_MODE="${FSR:=}"
-[ -x "$GAMESCOPE" ] && { [[ -x "$RRES" && -n "$FSR_MODE" ]] && CMD=("$GAMESCOPE" -f $("$RRES" -g "$FSR_MODE") -- "${CMD[@]}") || CMD=("$GAMESCOPE" -f -- "${CMD[@]}"); }
+: ${GAMESCOPE:=$(command -v gamescope)}; RRES=$(command -v rres); FSR_MODE="${FSR:=}"; [ -x "$GAMESCOPE" ] && { [[ -x "$RRES" && -n "$FSR_MODE" ]] && CMD=("$GAMESCOPE" -f $("$RRES" -g "$FSR_MODE") -- "${CMD[@]}") || CMD=("$GAMESCOPE" -f -- "${CMD[@]}"); }
 
 # dwarfs
-[ ! -f "$BINDIR/$BIN" ] && mkdir -p {"$PWD/files/groot-mnt","$PWD/files/groot-rw","$PWD/files/groot-work","$PWD/files/groot"} && dwarfs "$PWD/files/groot.dwarfs" "$PWD/files/groot-mnt" -o cache_image && fuse-overlayfs -o lowerdir="$PWD/files/groot-mnt",upperdir="$PWD/files/groot-rw",workdir="$PWD/files/groot-work" "$PWD/files/groot"
+[ ! -f "$BINDIR/$BIN" ] && mkdir -p {"$F/groot-mnt","$F/groot-rw","$F/groot-work","$F/groot"} && dwarfs "$F/groot.dwarfs" "$F/groot-mnt" -o cache_image && fuse-overlayfs -o lowerdir="$F/groot-mnt",upperdir="$F/groot-rw",workdir="$F/groot-work" "$F/groot"
 function cleanup {
-    cd "$ROOTDIR" && killall $(<"$PWD/files/binval.txt") && killall gamescope && sleep 10;
-    [ -d "$PWD/files/groot" ] && fusermount -u "$PWD/files/groot";
-    [ -d "$PWD/files/groot-mnt" ] && fusermount -u "$PWD/files/groot-mnt" && rm -d -f "$PWD/files/groot-mnt"
-}
+cd "$R" && wineserver -k && fuser -k "$F/groot-mnt"
+[ -d "$F/groot" ] && fusermount -u "$F/groot";
+[ -d "$F/groot-mnt" ] && fusermount -u "$F/groot-mnt" && rm -d -f "$F/groot-mnt"; }
+trap 'cleanup' EXIT SIGINT SIGTERM
 echo -e "\e[38;5;$((RANDOM%257))m" && cat << 'EOF'
        ⠀⠀⠀  ⠀ ⣴⣶⣤⡤⠦⣤⣀⣤⠆⠀⠀⠀⠀⠀⣈⣭⣿⣶⣿⣦⣼⣆
        ⠀⠀⠀ ⠀  ⠀⠉⠻⢿⣿⠿⣿⣿⣶⣦⠤⠄⡠⢾⣿⣿⡿⠋⠉⠉⠻⣿⣿⡛⣦
