34f000db4ea2b9884dcd8d8e2bdb533c0b114603
miocrime <106968863+miocrime@users.noreply.github.com>
2022-06-24 19:04:44 +0300

    Update start.w.sh

--- root-scripts/start.w.sh
+++ root-scripts/start.w.sh
@@ -2,7 +2,7 @@
 cd "$(dirname "$0")" || exit; [ "$EUID" = "0" ] && exit; export R="$PWD"; DWRFST="$R/dwarfsettings.sh"; RMT="$PWD/files/rumtricks.sh"; WHA="$PWD/files/wha.sh"; [ ! -e "$RMT" ] && cp /usr/bin/rumtricks "$RMT"; [ ! -e "$WHA" ] && cp /usr/bin/wha "$WHA"; export WINE_LARGE_ADDRESS_AWARE=1; 
 export WINEFSYNC=1; export WINEDLLOVERRIDES="mscoree=d;mshtml=d;"; 
 export BINDIR="$PWD/files/groot"; BIN="game.exe";
-[ -x "/bin/wine-tkg" ] && export WINE="$(command -v wine)" || export WINE="$BINDIR/wine/bin/wine"; CMD=("$WINE" "$BIN")
+[ -x "/bin/wine-tkg" ] && export WINE="$(command -v wine)" || export WINE="$BINDIR/wine/bin/wine"; CMD=("$WINE" "$BIN"); bash "$WHA" wine-tkg
 
 # gamescope/FSR
 : ${GAMESCOPE:=$(command -v gamescope)}; RRES=$(command -v rres); FSR_MODE="${FSR:=}"; [ -x "$GAMESCOPE" ] && { [[ -x "$RRES" && -n "$FSR_MODE" ]] && CMD=("$GAMESCOPE" -f $("$RRES" -g "$FSR_MODE") -- "${CMD[@]}") || CMD=("$GAMESCOPE" -f -- "${CMD[@]}"); }
@@ -14,7 +14,7 @@ cd "$OLDPWD" && bash "$DWRFST" unmount-prefix unmount-game
 }
 trap 'cleanup' EXIT SIGINT SIGTERM
 
-export WINEPREFIX="$PWD/files/data/prefix-tmp"; bash "$WHA" wine-tkg; bash "$RMT" isolation
+export WINEPREFIX="$PWD/files/data/prefix-tmp"; bash "$RMT" isolation
 
 echo -e "\e[38;5;$((RANDOM%257))m" && cat << 'EOF'
        ⠀⠀⠀     ⠀ ⣴⣶⣤⡤⠦⣤⣀⣤⠆⠀⠀⠀⠀⠀⣈⣭⣿⣶⣿⣦⣼⣆
