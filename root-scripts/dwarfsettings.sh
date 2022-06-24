ea813cd7302795fc5d2221df86d6d155c9a117f6
miocrime <106968863+miocrime@users.noreply.github.com>
2022-06-24 18:39:26 +0300

    Update dwarfsettings.sh

--- root-scripts/dwarfsettings.sh
+++ root-scripts/dwarfsettings.sh
@@ -10,23 +10,23 @@ mount-prefix() {
     [ -f "$JCPRF" ] && find "$JCDIR/prefix.dwarfs" -mtime +60 -type f -delete
     [ ! -d "$JCDIR" ] && mkdir -p "$JCDIR"; export WINEPREFIX="$HOME/jc141/prefix"
     [ ! -e "$RMT" ] && cp /usr/bin/rumtricks "$RMT";
-    [ ! -f "$JCPRF" ] && WINEPREFIX="$JCDIR/prefix" bash "$RMT" directx vcrun2003 vcrun2005 vcrun2008 vcrun2010 vcrun2012 vcrun2013 vcrun2015 vcrun2017 vcrun2019 && sleep 3 && mkdwarfs -l7 -B5 -i "$HOME/jc141/prefix" -o "$JCDIR/prefix.dwarfs" && rm -Rf "$WINEPREFIX"
+    [ ! -f "$JCPRF" ] && WINEPREFIX="$JCDIR/prefix" bash "$RMT" directx vcrun2003 vcrun2005 vcrun2008 vcrun2010 vcrun2012 vcrun2013 vcrun2015 vcrun2017 vcrun2019 && sleep 2 && mkdwarfs -l7 -B5 -i "$HOME/jc141/prefix" -o "$JCDIR/prefix.dwarfs" && rm -Rf "$WINEPREFIX"
     export WINEPREFIX="$PWD/files/data/prefix-tmp"
     [ ! -d "$WINEPREFIX" ] && mkdir -p {"$JCDIR/prefix-mnt","$PWD/files/data/user-data","$PWD/files/data/work","$PWD/files/data/prefix-tmp"} && dwarfs "$JCDIR/prefix.dwarfs" "$JCDIR/prefix-mnt" -o cache_image && fuse-overlayfs -o lowerdir="$JCDIR/prefix-mnt",upperdir="$PWD/files/data/user-data",workdir="$PWD/files/data/work" "$PWD/files/data/prefix-tmp";
     echo "DWRFS: Mounted prefix."
 }
 
 unmount-game() {
-    wineserver -k && fuser -k "$PWD/files/groot-mnt"
-    [ -d "$PWD/files/groot" ] && fusermount -u "$PWD/files/groot"
-    [ -d "$PWD/files/groot-mnt" ] && fusermount -u "$PWD/files/groot-mnt" && rm -d -f "$PWD/files/groot-mnt"
+    wineserver -k && sleep 2 && fuser -k "$PWD/files/groot-mnt"
+    [ -d "$PWD/files/groot" ] && sleep 2 && fusermount -u "$PWD/files/groot"
+    [ -d "$PWD/files/groot-mnt" ] && sleep 2 && fusermount -u "$PWD/files/groot-mnt" && rm -d -f "$PWD/files/groot-mnt"
     echo "DWRFS: Unmounted game."
 }
 
 unmount-prefix() {
-    wineserver -k && sleep 3 && fuser -k "$JCDIR/prefix-mnt"
-    sleep 3 && fusermount -u "$PWD/files/data/prefix-tmp" && rm -d -f "$PWD/files/data/prefix-tmp";
-    sleep 3 && fusermount -u "$JCDIR/prefix-mnt" && rm -d -f "$JCDIR/prefix-mnt"
+    wineserver -k && sleep 2 && fuser -k "$JCDIR/prefix-mnt"
+    sleep 2 && fusermount -u "$PWD/files/data/prefix-tmp" && rm -d -f "$PWD/files/data/prefix-tmp";
+    sleep 2 && fusermount -u "$JCDIR/prefix-mnt" && rm -d -f "$JCDIR/prefix-mnt"
     echo "DWRFS: Unmounted prefix."
 }
 
