c1f5dc838315e0f11d0078e0c46ace377d6c1b85
miocrime <106968863+miocrime@users.noreply.github.com>
2022-06-24 16:09:05 +0300

    Update start.n.sh

--- root-scripts/start.n.sh
+++ root-scripts/start.n.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
-cd "$(dirname "$0")" || exit; [ "$EUID" = "0" ] && exit; R="$PWD"; F="$PWD/files"; export HOME="$F/data"; export XDG_DATA_HOME="$F/data/.local"; export XDG_CONFIG_HOME="$F/data/.config"; mkdir -p {"$HOME","$XDG_CONFIG_HOME","$XDG_DATA_HOME"}
+cd "$(dirname "$0")" || exit; [ "$EUID" = "0" ] && exit; R="$PWD"; DWRFST="$R/dwarfsettings.sh"; export HOME="$F/data"; export XDG_DATA_HOME="$F/data/.local"; export XDG_CONFIG_HOME="$F/data/.config"; mkdir -p {"$HOME","$XDG_CONFIG_HOME","$XDG_DATA_HOME"}
 
-BINDIR="$F/groot"; BIN="game.bin"; CMD=(./"$BIN")
+BINDIR="$PWD/files/groot"; BIN="game.bin"; CMD=(./"$BIN")
 
 # gamescope/FSR
 : ${GAMESCOPE:=$(command -v gamescope)}; RRES=$(command -v rres); FSR_MODE="${FSR:=}"; [ -x "$GAMESCOPE" ] && { [[ -x "$RRES" && -n "$FSR_MODE" ]] && CMD=("$GAMESCOPE" -f $("$RRES" -g "$FSR_MODE") -- "${CMD[@]}") || CMD=("$GAMESCOPE" -f -- "${CMD[@]}"); }
 
 # dwarfs
-[ ! -f "$BINDIR/$BIN" ] && mkdir -p {"$F/groot-mnt","$F/groot-rw","$F/groot-work","$F/groot"} && dwarfs "$F/groot.dwarfs" "$F/groot-mnt" -o cache_image && fuse-overlayfs -o lowerdir="$F/groot-mnt",upperdir="$F/groot-rw",workdir="$F/groot-work" "$F/groot"
+bash "$DWRFST" mount-game
 function cleanup {
-cd "$R" && fuser -k "$F/groot-mnt"
-[ -d "$F/groot" ] && sleep 3 && fusermount -u "$F/groot";
-[ -d "$F/groot-mnt" ] && sleep 3 && fusermount -u "$F/groot-mnt" && rm -d -f "$F/groot-mnt"; }
+cd "$OLDPWD" && bash "$DWRFST" unmount-game
+}
 trap 'cleanup' EXIT SIGINT SIGTERM
+
 echo -e "\e[38;5;$((RANDOM%257))m" && cat << 'EOF'
        ⠀⠀⠀     ⠀ ⣴⣶⣤⡤⠦⣤⣀⣤⠆⠀⠀⠀⠀⠀⣈⣭⣿⣶⣿⣦⣼⣆
        ⠀⠀   ⠀ ⠀  ⠀⠉⠻⢿⣿⠿⣿⣿⣶⣦⠤⠄⡠⢾⣿⣿⡿⠋⠉⠉⠻⣿⣿⡛⣦
