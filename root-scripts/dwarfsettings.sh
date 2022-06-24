ef6d01ef1a5e718b90c76fcdf7b89cd512042ba6
miocrime <106968863+miocrime@users.noreply.github.com>
2022-06-24 18:04:08 +0300

    Update dwarfsettings.sh

--- root-scripts/dwarfsettings.sh
+++ root-scripts/dwarfsettings.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-JCDIR="$HOME/jc141"; JCPRF="$HOME/jc141/prefix.dwarfs";
+JCDIR="$HOME/jc141"; JCPRF="$HOME/jc141/prefix.dwarfs"; RMT="$JCDIR/rumtricks.sh";
 
 mount-game() {
     [ ! -f "$BINDIR/$BIN" ] && mkdir -p {"$PWD/files/groot-mnt","$PWD/files/groot-rw","$PWD/files/groot-work","$R/files/groot"} && dwarfs "$PWD/files/groot.dwarfs" "$PWD/files/groot-mnt" && fuse-overlayfs -o lowerdir="$PWD/files/groot-mnt",upperdir="$PWD/files/groot-rw",workdir="$PWD/files/groot-work" "$PWD/files/groot"
@@ -9,7 +9,8 @@ mount-game() {
 mount-prefix() {
     [ -f "$JCPRF" ] && find "$JCDIR/prefix.dwarfs" -mtime +60 -type f -delete
     [ ! -d "$JCDIR" ] && mkdir -p "$JCDIR"; export WINEPREFIX="$HOME/jc141/prefix"
-    [ ! -f "$JCPRF" ] && wineboot -e -u && sleep 5 && mkdwarfs -l7 -B5 -i "$HOME/jc141/prefix" -o "$JCDIR/prefix.dwarfs" && rm -Rf "$WINEPREFIX"
+    [ ! -e "$RMT" ] && cp /usr/bin/rumtricks "$RMT";
+    [ ! -f "$JCPRF" ] && WINEPREFIX="$JCDIR/prefix" bash "$RMT" directx vcrun2003 vcrun2005 vcrun2008 vcrun2010 vcrun2012 vcrun2013 vcrun2015 vcrun2017 vcrun2019 && sleep 3 && mkdwarfs -l7 -B5 -i "$HOME/jc141/prefix" -o "$JCDIR/prefix.dwarfs" && rm -Rf "$WINEPREFIX"
     export WINEPREFIX="$PWD/files/data/prefix-tmp"
     [ ! -d "$WINEPREFIX" ] && mkdir -p {"$JCDIR/prefix-mnt","$PWD/files/data/user-data","$PWD/files/data/work","$PWD/files/data/prefix-tmp"} && dwarfs "$JCDIR/prefix.dwarfs" "$JCDIR/prefix-mnt" -o cache_image && fuse-overlayfs -o lowerdir="$JCDIR/prefix-mnt",upperdir="$PWD/files/data/user-data",workdir="$PWD/files/data/work" "$PWD/files/data/prefix-tmp";
     echo "DWRFS: Mounted prefix."
