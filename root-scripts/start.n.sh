4e7125f91adc19aaf3fc636b8e103c1518339332
Darren Boyd <92080679+boyddarren@users.noreply.github.com>
2022-06-25 12:43:13 +0530

    start.n: fixed reference to missing "$F" var

--- root-scripts/start.n.sh
+++ root-scripts/start.n.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
-cd "$(dirname "$0")" || exit; [ "$EUID" = "0" ] && exit; R="$PWD"; DWRFST="$R/dwarfsettings.sh"; export HOME="$F/data"; export XDG_DATA_HOME="$F/data/.local"; export XDG_CONFIG_HOME="$F/data/.config"; mkdir -p {"$HOME","$XDG_CONFIG_HOME","$XDG_DATA_HOME"}
+cd "$(dirname "$(readlink -f "$0")")" || exit; [ "$EUID" = "0" ] && exit; R="$PWD"; DWRFST="$R/dwarfsettings.sh"; export HOME="$R/files/data"; export XDG_DATA_HOME="$R/files/data/.local"; export XDG_CONFIG_HOME="$R/files/data/.config"; mkdir -p {"$HOME","$XDG_CONFIG_HOME","$XDG_DATA_HOME"}
 
-BINDIR="$PWD/files/groot"; BIN="game.bin"; CMD=(./"$BIN")
+BINDIR="$R/files/groot"; BIN="game.bin"; CMD=(./"$BIN")
 
 # gamescope/FSR
 : ${GAMESCOPE:=$(command -v gamescope)}; RRES=$(command -v rres); FSR_MODE="${FSR:=}"; [ -x "$GAMESCOPE" ] && { [[ -x "$RRES" && -n "$FSR_MODE" ]] && CMD=("$GAMESCOPE" -f $("$RRES" -g "$FSR_MODE") -- "${CMD[@]}") || CMD=("$GAMESCOPE" -f -- "${CMD[@]}"); }
 
 # dwarfs
 bash "$DWRFST" mount-game
-function cleanup {
-cd "$OLDPWD" && bash "$DWRFST" unmount-game
-}
+function cleanup { cd "$OLDPWD" && bash "$DWRFST" unmount-game; }
 trap 'cleanup' EXIT SIGINT SIGTERM
 
 echo -e "\e[38;5;$((RANDOM%257))m" && cat << 'EOF'
