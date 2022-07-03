5b915d3939e438438cbedf04de80dcc16161f825
Darren Boyd <92080679+boyddarren@users.noreply.github.com>
2022-07-03 19:18:35 +0530

    Update start.w.sh

--- root-scripts/start.w.sh
+++ root-scripts/start.w.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
-cd "$(dirname "$(readlink -f "$0")")" || exit; [ "$EUID" = "0" ] && exit; export R="$PWD"; DWRFST="$R/dwarfsettings.sh"; RMT="$PWD/files/rumtricks.sh"; WHA="$PWD/files/wha.sh"; 
+cd "$(dirname "$(readlink -f "$0")")" || exit; [ "$EUID" = "0" ] && exit; export R="$PWD"; DWRFST="$R/dwarfsettings.sh"; RMT="$PWD/files/rumtricks.sh"; WHA="$PWD/files/wha.sh";
 [ ! -e "$RMT" ] && cp /usr/bin/rumtricks "$RMT"; [ ! -e "$WHA" ] && cp /usr/bin/wha "$WHA"; export WINE_LARGE_ADDRESS_AWARE=1;
-export WINEFSYNC=1; export WINEDLLOVERRIDES="mscoree=d;mshtml=d;"; 
+export WINEFSYNC=1; export WINEDLLOVERRIDES="mscoree=d;mshtml=d;";
 export BINDIR="$PWD/files/groot"; BIN="game.exe";
-export WINE="$(command -v wine)"; [ ! -x "$WINE" ] && export WINE="$BINDIR/wine/bin/wine"; CMD=("$WINE" "$BIN");
+export WINE="$(command -v wine-tkg || command -v wine-ge || command -v wine-tkg-nomingw )"; [ ! -x "$WINE" ] && export WINE="$BINDIR/wine/bin/wine"; bash "$WHA" "$(basename $WINE)"; CMD=("$WINE" "$BIN");
 
 # gamescope/FSR
 : ${GAMESCOPE:=$(command -v gamescope)}; [ -x "$GAMESCOPE" ] && CMD=("$GAMESCOPE" -f -- "${CMD[@]}");
-
-# Custom WINE (wine-tkg, wine-ge, wine-tkg-nomingw)
-CWINE="wine-tkg"; bash "$WHA" "$CWINE"
 
 # dwarfs
 bash "$DWRFST" mount-game; bash "$DWRFST" mount-prefix;
@@ -43,3 +40,4 @@ EOF
 echo -e "\e[0m"
 [ "${DBG:=0}" = "1" ] || exec &>/dev/null
 cd "$BINDIR"; "${CMD[@]}" "$@"
+
