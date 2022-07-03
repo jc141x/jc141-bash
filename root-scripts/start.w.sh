dbd4879bfb902d707fe36afd532b314336a9fe12
miocrime <106968863+miocrime@users.noreply.github.com>
2022-07-03 20:05:56 +0300

    Update start.w.sh

--- root-scripts/start.w.sh
+++ root-scripts/start.w.sh
@@ -2,7 +2,7 @@
 cd "$(dirname "$(readlink -f "$0")")" || exit; [ "$EUID" = "0" ] && exit; export R="$PWD"; DWRFST="$R/dwarfsettings.sh"; RMT="$PWD/files/rumtricks.sh"; WHA="$PWD/files/wha.sh";
 [ ! -e "$RMT" ] && cp /usr/bin/rumtricks "$RMT"; [ ! -e "$WHA" ] && cp /usr/bin/wha "$WHA"; export WINE_LARGE_ADDRESS_AWARE=1;
 export WINEFSYNC=1; export WINEDLLOVERRIDES="mscoree=d;mshtml=d;";
-export BINDIR="$PWD/files/groot"; BIN="game.exe";
+export BINDIR="$PWD/files/groot"; BIN="game.exe"; bash "$DWRFST" mount-game
 
 # WINE handler (choices: wine-tkg, wine-ge, wine-tkg-nomingw)
 _WINE="wine-tkg"; bash "$WHA" "$_WINE"; [ -x "$BINDIR/wine/bin/wine" ] && export WINE="$BINDIR/wine/bin/wine" || export WINE="$(command -v wine)"; CMD=("$WINE" "$BIN");
@@ -11,7 +11,7 @@ _WINE="wine-tkg"; bash "$WHA" "$_WINE"; [ -x "$BINDIR/wine/bin/wine" ] && export
 : ${GAMESCOPE:=$(command -v gamescope)}; [ -x "$GAMESCOPE" ] && CMD=("$GAMESCOPE" -f -- "${CMD[@]}");
 
 # dwarfs
-bash "$DWRFST" mount-game; bash "$DWRFST" mount-prefix; export WINEPREFIX="$PWD/files/data/prefix-tmp"
+bash "$DWRFST" mount-prefix; export WINEPREFIX="$PWD/files/data/prefix-tmp"
 
 # Rumtricks
 bash "$RMT" isolation
