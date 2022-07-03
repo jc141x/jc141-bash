ad75a0f11bd66e021e1a96d8529315a80f695407
Darren Boyd <92080679+boyddarren@users.noreply.github.com>
2022-07-03 18:58:33 +0530

    start.w.sh: tweaks
    - look for system WINE, then look for shipped WINE
    - ability to choose Custom WINE for `wha` (wine-tkg by default)
    - Rumtricks line gets it's own "section" (so more tricks can be added as per need)
    - Cleanup trap gets it's own section (for extending the cleanup function, like for older games which use fluidsynth for MIDI sounds)

--- root-scripts/start.w.sh
+++ root-scripts/start.w.sh
@@ -3,17 +3,24 @@ cd "$(dirname "$(readlink -f "$0")")" || exit; [ "$EUID" = "0" ] && exit; export
 [ ! -e "$RMT" ] && cp /usr/bin/rumtricks "$RMT"; [ ! -e "$WHA" ] && cp /usr/bin/wha "$WHA"; export WINE_LARGE_ADDRESS_AWARE=1;
 export WINEFSYNC=1; export WINEDLLOVERRIDES="mscoree=d;mshtml=d;"; 
 export BINDIR="$PWD/files/groot"; BIN="game.exe";
-[ -x "/bin/wine-tkg" ] && export WINE="$(command -v wine)" || export WINE="$BINDIR/wine/bin/wine"; CMD=("$WINE" "$BIN");
+export WINE="$(command -v wine)"; [ ! -x "$WINE" ] && export WINE="$BINDIR/wine/bin/wine"; CMD=("$WINE" "$BIN");
 
 # gamescope/FSR
 : ${GAMESCOPE:=$(command -v gamescope)}; [ -x "$GAMESCOPE" ] && CMD=("$GAMESCOPE" -f -- "${CMD[@]}");
 
+# Custom WINE (wine-tkg, wine-ge, wine-tkg-nomingw)
+CWINE="wine-tkg"; bash "$WHA" "$CWINE"
+
 # dwarfs
-bash "$DWRFST" mount-game; bash "$WHA" wine-tkg; bash "$DWRFST" mount-prefix;
+bash "$DWRFST" mount-game; bash "$DWRFST" mount-prefix;
+export WINEPREFIX="$PWD/files/data/prefix-tmp"
+
+# Rumtricks
+bash "$RMT" isolation
+
+# Cleanup (runs when game exits/crashes)
 function cleanup { cd "$OLDPWD" && bash "$DWRFST" unmount-prefix unmount-game; }
 trap 'cleanup' EXIT SIGINT SIGTERM
-
-export WINEPREFIX="$PWD/files/data/prefix-tmp"; bash "$RMT" isolation
 
 echo -e "\e[38;5;$((RANDOM%257))m" && cat << 'EOF'
                      ⣐⢕⢕⢕⢕⢕⢕⢕⢕⠅⢗⢕⢕⢕⢕⢕⢕⢕⠕⠕⢕⢕⢕⢕⢕⢕⢕⢕⢕
