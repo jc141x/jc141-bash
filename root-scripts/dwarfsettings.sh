0ee05b5cd922f76819ace10dfa96f163d74fa2b0
miocrime <106968863+miocrime@users.noreply.github.com>
2022-07-04 22:11:40 +0300

    Update dwarfsettings.sh

--- root-scripts/dwarfsettings.sh
+++ root-scripts/dwarfsettings.sh
@@ -3,7 +3,8 @@ RMTDIR="${XDG_DATA_HOME:-$HOME/.local/share}/rumtricks"; RMTCONTENT="$RMTDIR/rum
 PRF="$RMTDIR/prefix.dwarfs"; RMT="$RMTDIR/rumtricks.sh";
 
 mount-game() {
-[ -d "$PWD/files/groot" ] && echo "DWRFS: Mounting path exists." && [ ! ls -A "$PWD/files/groot" ] && echo "DWRFS: Game is already mounted or extracted." || mkdir -p {"$PWD/files/groot-mnt","$PWD/files/groot-rw","$PWD/files/groot-work","$PWD/files/groot"} && dwarfs "$PWD/files/groot.dwarfs" "$PWD/files/groot-mnt" && fuse-overlayfs -o lowerdir="$PWD/files/groot-mnt",upperdir="$PWD/files/groot-rw",workdir="$PWD/files/groot-work" "$PWD/files/groot" && echo "DWRFS: Mounted game."; }
+[ -d "$PWD/files/groot" ] && echo "DWRFS: Mounting path exists." && [ "$( ls -A "$PWD/files/groot")" ] && echo "DWRFS: Game is already mounted or extracted." && exit
+mkdir -p {"$PWD/files/groot-mnt","$PWD/files/groot-rw","$PWD/files/groot-work","$PWD/files/groot"} && dwarfs "$PWD/files/groot.dwarfs" "$PWD/files/groot-mnt" && fuse-overlayfs -o lowerdir="$PWD/files/groot-mnt",upperdir="$PWD/files/groot-rw",workdir="$PWD/files/groot-work" "$PWD/files/groot" && echo "DWRFS: Mounted game."; }
 
 mount-prefix() {
 # downloading
@@ -19,17 +20,18 @@ DLRLS="$(echo "$RMTRLS" | awk -F '["]' '/"browser_download_url":/ && /tar.lzma/ 
 [ ! -d "$WINEPREFIX" ] && mkdir -p {"$RMTDIR/prefix-mnt","$PWD/files/data/user-data","$PWD/files/data/work","$PWD/files/data/prefix-tmp"} && dwarfs "$RMTDIR/prefix.dwarfs" "$RMTDIR/prefix-mnt" -o cache_image && fuse-overlayfs -o lowerdir="$RMTDIR/prefix-mnt",upperdir="$PWD/files/data/user-data",workdir="$PWD/files/data/work" "$PWD/files/data/prefix-tmp";
 echo "DWRFS: Mounted prefix."; }
 
-unmount-game() { wineserver -k && killall gamescope && sleep 1 && fuser -k "$PWD/files/groot-mnt"
+unmount-game() { wineserver -k && killall gamescope && fuser -k "$PWD/files/groot-mnt"
 fusermount -u -z "$PWD/files/groot"
 fusermount -u -z "$PWD/files/groot-mnt" && rm -d -f "$PWD/files/groot-mnt" && rm -d "$PWD/files/groot-work"
 echo "DWRFS: Unmounted game."; }
 
-unmount-prefix() { wineserver -k && sleep 2 && fuser -k "$RMTDIR/prefix-mnt"
+unmount-prefix() { wineserver -k && fuser -k "$RMTDIR/prefix-mnt"
 fusermount -u -z "$PWD/files/data/prefix-tmp" && rm -d -f "$PWD/files/data/prefix-tmp";
 fusermount -u -z "$RMTDIR/prefix-mnt" && rm -d -f "$RMTDIR/prefix-mnt"
 echo "DWRFS: Unmounted prefix."; }
 
-extract-game() { [ -d "$PWD/files/groot" ] && echo "DWRFS: Extraction path exists." && [ ! ls -A "$PWD/files/groot" ] && echo "DWRFS: Game is already extracted." && exit || rm -d "$PWD/files/groot" && echo "DWRFS: Extracting game, this may take a while. Do not cancel the process or the files will be incomplete."; tstart="$(date +%s)" && mkdir "$PWD/files/groot" && dwarfsextract -i "$PWD/files/groot.dwarfs" -o "$PWD/files/groot" && tend="$(date +%s)"; elapsed="$((tend - tstart))" && echo "done in $((elapsed / 60)) min and $((elapsed % 60)) sec"; }
+extract-game() { [ -d "$PWD/files/groot" ] && echo "DWRFS: Extraction path exists." && [ "$( ls -A "$PWD/files/groot")" ] && echo "DWRFS: Game is already mounted or extracted." && exit
+rm -d "$PWD/files/groot" && echo "DWRFS: Extracting game, this may take a while. Do not cancel the process or the files will be incomplete."; tstart="$(date +%s)" && mkdir "$PWD/files/groot" && dwarfsextract -i "$PWD/files/groot.dwarfs" -o "$PWD/files/groot" && tend="$(date +%s)"; elapsed="$((tend - tstart))" && echo "DWRFS: Done in $((elapsed / 60)) min and $((elapsed % 60)) sec"; }
 
 for i in "$@"; do
     # Check if function exists
