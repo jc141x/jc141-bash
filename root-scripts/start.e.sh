48f447a16233f6ec6ac926e7bb97876754868c34
miocrime <106968863+miocrime@users.noreply.github.com>
2022-07-10 15:19:42 +0300

    Update start.e.sh

--- root-scripts/start.e.sh
+++ root-scripts/start.e.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 cd "$(dirname "$(readlink -f "$0")")" || exit; [ "$EUID" = "0" ] && exit; F="$PWD/files"; 
-export HOME="$F/data"; export XDG_DATA_HOME="$F/data/.local"; export XDG_CONFIG_HOME="$F/data/.config"; mkdir -p {"$HOME","$XDG_CONFIG_HOME","$XDG_DATA_HOME"};
-
+export HOME="$F/data"; export XDG_DATA_HOME="$F/data/.local"; export XDG_CONFIG_HOME="$F/data/.config"; [ ! -d "$F/data" ] && cp -r "$F/data-backup" "$F/data"; mkdir -p {"$HOME","$XDG_CONFIG_HOME","$XDG_DATA_HOME"};
 YUZU="$(command -v yuzu)"; export QT_QPA_PLATFORM=xcb
 BINDIR="$F/groot"; BIN="game.nsp"; CMD=("$YUZU" "$BIN");
-
-# copy backup data to real path
-[ ! -d "$F/data" ] && cd "$F" && cp -r "data-backup" "data"
 
 echo -e "\e[38;5;$((RANDOM%257))m" && cat << 'EOF'
                      ⣐⢕⢕⢕⢕⢕⢕⢕⢕⠅⢗⢕⢕⢕⢕⢕⢕⢕⠕⠕⢕⢕⢕⢕⢕⢕⢕⢕⢕
