af21b881b16f007c91eff62afe63d0e6396ec809
miocrime <106968863+miocrime@users.noreply.github.com>
2022-08-09 00:31:18 +0300

    Update dwarfsettings.sh

--- root-scripts/dwarfsettings.sh
+++ root-scripts/dwarfsettings.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 [ ! -x "$(command -v dwarfs)" ] && echo "dwarfs not installed" && exit; [ ! -x "$(command -v fuse-overlayfs)" ] && echo "fuse-overlayfs not installed" && exit
-RMTDIR="${XDG_DATA_HOME:-$HOME/.local/share}/rumtricks"; [ -d "$RMTDIR" ] && mkdir -p "$RMTDIR"; RMTCONTENT="$RMTDIR/rumtricks-content"; RMTARCH="rumtricks-content.tar.lzma";
-PRF="$RMTDIR/prefix.dwarfs"; RMT="$RMTDIR/rumtricks.sh";
+JCD="${XDG_DATA_HOME:-$HOME/.local/share}/jc141"; [ -d "$JCD" ] && mkdir -p "$JCD"; BASE="$JCD/base"; BASEARCH="base.tar.lzma";
+PRF="$JCD/prefix.dwarfs"; BASEINSTALL="$JCD/base/install.sh";
 
 mount-game() { unmount-game &> /dev/null;
 [ -d "$PWD/files/groot" ] && echo -n "mounting path exists | " && [ "$( ls -A "$PWD/files/groot")" ] && echo -n "game is already mounted or extracted | " && exit
@@ -9,16 +9,17 @@ mkdir -p {"$PWD/files/groot-mnt","$PWD/files/groot-rw","$PWD/files/groot-work","
 
 mount-prefix() { unmount-prefix &> /dev/null;
 # downloading
-RMTRLS="$(curl -s https://api.github.com/repos/jc141x/rumtricks/releases/latest)"
-DLRLS="$(echo "$RMTRLS" | awk -F '["]' '/"browser_download_url":/ && /tar.lzma/ {print $4}')"
-[ ! -f "$RMTDIR/$RMTARCH" ] && [ ! -d "$RMTCONTENT" ] && curl -L "$DLRLS" -o "$RMTDIR/$RMTARCH" && [ ! -f "$RMTDIR/$RMTARCH" ] && echo -n "download failed | " && exit || [ ! -d "$RMTCONTENT" ] && tar -xvf "$RMTDIR/$RMTARCH" -C "$RMTDIR" > /dev/null && rm -Rf "$RMTDIR/$RMTARCH"
+BASELNK="$(curl -s https://api.github.com/repos/jc141x/WINDEP/releases/latest)"
+DLRLS="$(echo "$BASELNK" | awk -F '["]' '/"browser_download_url":/ && /tar.lzma/ {print $4}')"
+[ ! -f "$JCD/$BASEARCH" ] && [ ! -d "$BASE" ] && curl -L "$DLRLS" -o "$JCD/$BASEARCH" && [ ! -f "$JCD/$BASEARCH" ] && echo -n "download failed | " && exit || [ ! -d "$BASE" ] && tar -xvf "$JCD/$BASEARCH" -C "$JCD" > /dev/null
 
 # prefix generation
-[ -f "$PRF" ] && find "$RMTDIR/prefix.dwarfs" -mtime +60 -type f -delete; export WINEPREFIX="$RMTDIR/prefix"; [ ! -f "$RMT" ] && cp "$PWD/files/rumtricks.sh" "$RMT";
-[ ! -f "$PRF" ] && WINEPREFIX="$RMTDIR/prefix" bash "$RMT" isolation directx vcrun && sleep 2 && mkdwarfs -l7 -B5 -i "$RMTDIR/prefix" -o "$RMTDIR/prefix.dwarfs" && rm -Rf "$WINEPREFIX"
+export WINEPREFIX="$JCD/prefix"
+[ -f "$PRF" ] && find "$JCD/prefix.dwarfs" -mtime +60 -type f -delete && tar -xvf "$JCD/$BASEARCH" -C "$JCD"
+[ ! -f "$PRF" ] && WINEPREFIX="$JCD/prefix" bash "$BASEINSTALL" && sleep 2 && mkdwarfs -l7 -B5 -i "$JCD/prefix" -o "$JCD/prefix.dwarfs" && rm -Rf "$WINEPREFIX" && rm -Rf "$BASE";
 
 # mounting prefix
-[ ! -d "$WINEPREFIX" ] && mkdir -p {"$RMTDIR/prefix-mnt","$PWD/files/data/user-data","$PWD/files/data/work","$PWD/files/data/prefix-tmp"} && dwarfs "$RMTDIR/prefix.dwarfs" "$RMTDIR/prefix-mnt" -o cache_image && fuse-overlayfs -o lowerdir="$RMTDIR/prefix-mnt",upperdir="$PWD/files/data/user-data",workdir="$PWD/files/data/work" "$PWD/files/data/prefix-tmp";
+[ ! -d "$WINEPREFIX" ] && mkdir -p {"$JCD/prefix-mnt","$PWD/files/data/user-data","$PWD/files/data/work","$PWD/files/data/prefix-tmp"} && dwarfs "$JCD/prefix.dwarfs" "$JCD/prefix-mnt" -o cache_image && fuse-overlayfs -o lowerdir="$JCD/prefix-mnt",upperdir="$PWD/files/data/user-data",workdir="$PWD/files/data/work" "$PWD/files/data/prefix-tmp";
 echo -n "mounted prefix | "; }
 
 unmount-game() { wineserver -k && killall gamescope && fuser -k "$PWD/files/groot-mnt"
@@ -26,9 +27,9 @@ fusermount3 -u -z "$PWD/files/groot"
 fusermount3 -u -z "$PWD/files/groot-mnt" && rm -d -f "$PWD/files/groot-mnt" && rm -d -f "$PWD/files/groot-work"
 echo -n "unmounted game | "; }
 
-unmount-prefix() { wineserver -k && fuser -k "$RMTDIR/prefix-mnt"
+unmount-prefix() { wineserver -k && fuser -k "$JCD/prefix-mnt"
 fusermount3 -u -z "$PWD/files/data/prefix-tmp" && rm -d -f "$PWD/files/data/prefix-tmp";
-fusermount3 -u -z "$RMTDIR/prefix-mnt" && rm -d -f "$RMTDIR/prefix-mnt"
+fusermount3 -u -z "$JCD/prefix-mnt" && rm -d -f "$JCD/prefix-mnt"
 echo -n "unmounted prefix | "; }
 
 extract-game() { [ -d "$PWD/files/groot" ] && echo -n "extraction path exists | " && [ "$( ls -A "$PWD/files/groot")" ] && echo -n "game is already mounted or extracted | " && exit
