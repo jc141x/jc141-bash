2c8806704255b483ba1ccd79b48ba1c6c7b82d7e
dblmask <59618912+dblmask@users.noreply.github.com>
2022-08-07 21:24:54 -0500

    mount wine as game and prefix
    remove wha.sh dependency and mount/unmount wine using new functions in dwarfssettings.sh and setting the PATH accordinly

--- root-scripts/start.w.sh
+++ root-scripts/start.w.sh
@@ -3,14 +3,14 @@
 cd "$(dirname "$(readlink -f "$0")")" || exit; [ "$EUID" = "0" ] && exit; export R="$PWD"; DWRF="$R/dwarfsettings.sh"; RMT="$PWD/files/rumtricks.sh"; WHA="$PWD/files/wha.sh";
 [ ! -e "$RMT" ] && cp /usr/bin/rumtricks "$RMT"; [ ! -e "$WHA" ] && cp /usr/bin/wha "$WHA"; export WINE_LARGE_ADDRESS_AWARE=1; export WINEFSYNC=1; export WINEDLLOVERRIDES="mshtml=d;";
 
-# dwarfs
-bash "$DWRF" mount-game; bash "$DWRF" mount-prefix
-
 # prefix + game files
 export WINEPREFIX="$PWD/files/data/prefix-tmp"; export BINDIR="$PWD/files/groot"; BIN="game.exe"
 
 # WINE handler (choices: wine-tkg, wine-ge, wine-tkg-nomingw)
-_WINE="wine-tkg"; bash "$WHA" "$_WINE"; [ -x "$BINDIR/wine/bin/wine" ] && export WINE="$BINDIR/wine/bin/wine" || export WINE="$(command -v wine)"; CMD=("$WINE" "$BIN");
+_WINE="wine-tkg"; bash "$DWRF" "$_WINE"; [ -x "$PWD/wine/bin/wine" ] && export PATH="$PWD/wine/bin:$PATH"; export WINE="$(command -v wine)"; CMD=("$WINE" "$BIN");
+
+# dwarfs
+bash "$DWRF" mount-game; bash "$DWRF" mount-prefix
 
 # gamescope
 : ${GAMESCOPE:=$(command -v gamescope)}; [ -x "$GAMESCOPE" ] && CMD=("$GAMESCOPE" -f -- "${CMD[@]}");
@@ -19,7 +19,7 @@ _WINE="wine-tkg"; bash "$WHA" "$_WINE"; [ -x "$BINDIR/wine/bin/wine" ] && export
 bash "$RMT" dxvk
 
 # Cleanup (runs when game exits/crashes)
-function cleanup { cd "$OLDPWD" && bash "$DWRF" unmount-prefix unmount-game; }
+function cleanup { cd "$OLDPWD" && bash "$DWRF" unmount-prefix unmount-game unmount-wine; }
 trap 'cleanup' EXIT INT SIGINT SIGTERM
 
 echo -e "\e[38;5;$((RANDOM%257))m" && cat << 'EOF'
