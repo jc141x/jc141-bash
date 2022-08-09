535b5d6e960f69ae3c94b00fef680c82f66d5aa7
miocrime <106968863+miocrime@users.noreply.github.com>
2022-08-09 18:37:26 +0300

    Update start.w.sh

--- root-scripts/start.w.sh
+++ root-scripts/start.w.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 [ ! -x "$(command -v dwarfs)" ] && echo "dwarfs not installed" && exit; [ ! -x "$(command -v fuse-overlayfs)" ] && echo "fuse-overlayfs not installed" && exit
-cd "$(dirname "$(readlink -f "$0")")" || exit; [ "$EUID" = "0" ] && exit; export R="$PWD"; DWRF="$R/dwarfsettings.sh"; WHA="$R/files/wha.sh";
-[ ! -e "$WHA" ] && cp /usr/bin/wha "$WHA"; export WINE_LARGE_ADDRESS_AWARE=1; export WINEFSYNC=1; export WINEDLLOVERRIDES="mshtml=d;";
+cd "$(dirname "$(readlink -f "$0")")" || exit; [ "$EUID" = "0" ] && exit; export R="$PWD"; DWRF="$R/dwarfsettings.sh"; WHA="$R/files/wha.sh"; VLK="$R/files/vulkan.sh"
+[ ! -e "$WHA" ] && cp /opt/jc141-bash/wha "$WHA"; [ ! -e "$VLK" ] && cp /opt/jc141-bash/vulkan "$VLK"; export WINE_LARGE_ADDRESS_AWARE=1; export WINEFSYNC=1; export WINEDLLOVERRIDES="mshtml=d;";
 
 bash "$DWRF" mount-game; bash "$DWRF" mount-prefix
 
