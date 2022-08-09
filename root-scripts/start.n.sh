c370d1556cdbbb3b4e55ad2614ed8facb18b5ca4
miocrime <106968863+miocrime@users.noreply.github.com>
2022-08-10 00:42:24 +0300

    Update start.n.sh

--- root-scripts/start.n.sh
+++ root-scripts/start.n.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 [ ! -x "$(command -v dwarfs)" ] && echo "dwarfs not installed" && exit; [ ! -x "$(command -v fuse-overlayfs)" ] && echo "fuse-overlayfs not installed" && exit
-cd "$(dirname "$(readlink -f "$0")")" || exit; [ "$EUID" = "0" ] && exit; R="$PWD"; DWRF="$R/dwarfsettings.sh"; 
-export HOME="$R/files/data"; export XDG_DATA_HOME="$R/files/data/.local"; export XDG_CONFIG_HOME="$R/files/data/.config"; mkdir -p {"$HOME","$XDG_CONFIG_HOME","$XDG_DATA_HOME"}; BIND_INTERFACE=lo; BIND_EXCLUDE=10.,172.16.,192.168.; LD_PRELOAD="/home/$USER/.local/share/jc141/bindToInterface.so"
+cd "$(dirname "$(readlink -f "$0")")" || exit; [ "$EUID" = "0" ] && exit; R="$PWD"; DWRF="$R/dwarfsettings.sh";
 
 BINDIR="$R/files/groot"; BIN="game.bin"; CMD=(./"$BIN")
 
@@ -31,4 +30,6 @@ echo -e "\e[38;5;$((RANDOM%257))m" && cat << 'EOF'
 EOF
 echo -e "\e[0m"
 [ "${DBG:=0}" = "1" ] || exec &>/dev/null
+BIND_INTERFACE=lo; BIND_EXCLUDE=10.,172.16.,192.168.; LD_PRELOAD="/home/$USER/.local/share/jc141/bindToInterface.so"
+export HOME="$R/files/data"; export XDG_DATA_HOME="$R/files/data/.local"; export XDG_CONFIG_HOME="$R/files/data/.config"; mkdir -p {"$HOME","$XDG_CONFIG_HOME","$XDG_DATA_HOME"};
 cd "$BINDIR"; "${CMD[@]}" "$@"
