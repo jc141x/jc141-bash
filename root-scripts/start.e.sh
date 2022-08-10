a1d4dfac6ad58ddc9256e55d3ffaefe283621d78
miocrime <106968863+miocrime@users.noreply.github.com>
2022-08-10 15:21:28 +0300

    Update start.e.sh

--- root-scripts/start.e.sh
+++ root-scripts/start.e.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd "$(dirname "$(readlink -f "$0")")" || exit; [ "$EUID" = "0" ] && exit; F="$PWD/files"; 
 export HOME="$F/data"; export XDG_DATA_HOME="$F/data/.local"; export XDG_CONFIG_HOME="$F/data/.config"; [ ! -d "$F/data" ] && cp -r "$F/data-backup" "$F/data"; mkdir -p {"$HOME","$XDG_CONFIG_HOME","$XDG_DATA_HOME"};
-YUZU="$(command -v yuzu)"; BINDIR="$F/groot"; BIN="game.nsp"; CMD=("$YUZU" "$BIN"); BIND_INTERFACE=lo; LD_PRELOAD="/home/$USER/.local/share/jc141/bindToInterface.so"
+YUZU="$(command -v yuzu)"; BINDIR="$F/groot"; BIN="game.nsp"; CMD=("$YUZU" "$BIN");
 
 echo -e "\e[38;5;$((RANDOM%257))m" && cat << 'EOF'
                      ⣐⢕⢕⢕⢕⢕⢕⢕⢕⠅⢗⢕⢕⢕⢕⢕⢕⢕⠕⠕⢕⢕⢕⢕⢕⢕⢕⢕⢕
@@ -22,6 +22,6 @@ echo -e "\e[38;5;$((RANDOM%257))m" && cat << 'EOF'
                   jc141 - 1337x.to -⠀rumpowered.org
 EOF
 echo -e "\e[0m"
-
 [ "${DBG:=0}" = "1" ] || exec &>/dev/null
+BIND_INTERFACE=lo; LD_PRELOAD="/home/$USER/.local/share/jc141/bindToInterface.so"
 cd "$BINDIR"; exec "${CMD[@]}" "$@"
