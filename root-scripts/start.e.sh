fa5fafd4a2a213f81374eee337e61782435490a8
miocrime <106968863+miocrime@users.noreply.github.com>
2022-08-22 19:12:27 +0300

    Update start.e.sh

--- root-scripts/start.e.sh
+++ root-scripts/start.e.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 cd "$(dirname "$(readlink -f "$0")")" || exit; [ "$EUID" = "0" ] && exit; F="$PWD/files"; 
 export HOME="$F/data"; export XDG_DATA_HOME="$F/data/.local"; export XDG_CONFIG_HOME="$F/data/.config"; [ ! -d "$F/data" ] && cp -r "$F/data-backup" "$F/data"; mkdir -p {"$HOME","$XDG_CONFIG_HOME","$XDG_DATA_HOME"};
+LOGO="$PWD/files/logo.txt.gz"; [ ! -e "$LOGO" ] && cp /opt/jc141-bash/logo.txt.gz "$LOGO";
 YUZU="$(command -v yuzu)"; BINDIR="$F/groot"; BIN="game.nsp"; CMD=("$YUZU" "$BIN");
 
 [ "${DBG:=0}" = "1" ] || exec &>/dev/null
 BIND_INTERFACE=lo; LD_PRELOAD="/home/$USER/.local/share/jc141/bindToInterface.so"
 
-zcat "$PWD/files/logo.txt.gz"; cd "$BINDIR"; exec "${CMD[@]}" "$@"
+zcat "$LOGO; cd "$BINDIR"; exec "${CMD[@]}" "$@"
