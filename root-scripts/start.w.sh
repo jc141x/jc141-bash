ea31074b2c31202374fa17e89e9aa7cba9e3a6c1
miocrime <106968863+miocrime@users.noreply.github.com>
2022-08-23 10:51:08 +0300

    Update start.w.sh

--- root-scripts/start.w.sh
+++ root-scripts/start.w.sh
@@ -17,7 +17,8 @@ bash "$VLK" dxvk
 function cleanup { cd "$OLDPWD" && bash "$DWRF" unmount-prefix unmount-game; }
 trap 'cleanup' EXIT INT SIGINT SIGTERM
 
-zcat "$LOGO"; [ "${DBG:=0}" = "1" ] || exec &>/dev/null
+zcat "$LOGO"
+[ "${DBG:=0}" = "1" ] || exec &>/dev/null
 export BIND_INTERFACE=lo; export BIND_EXCLUDE=10.,172.16.,192.168.; export LD_PRELOAD="/home/$USER/.local/share/jc141/bindToInterface.so"
 
 cd "$BINDIR"; "${CMD[@]}" "$@"
