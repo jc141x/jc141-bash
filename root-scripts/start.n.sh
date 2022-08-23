9b28a83ea9496471832af0b3a659ddbe19a5a64f
miocrime <106968863+miocrime@users.noreply.github.com>
2022-08-23 10:51:02 +0300

    Update start.n.sh

--- root-scripts/start.n.sh
+++ root-scripts/start.n.sh
@@ -10,7 +10,8 @@ bash "$DWRF" mount-game
 function cleanup { cd "$OLDPWD" && bash "$DWRF" unmount-game; }
 trap 'cleanup' EXIT INT SIGINT SIGTERM
 
-zcat "$LOGO"; [ "${DBG:=0}" = "1" ] || exec &>/dev/null
+zcat "$LOGO"
+[ "${DBG:=0}" = "1" ] || exec &>/dev/null
 export BIND_INTERFACE=lo; export BIND_EXCLUDE=10.,172.16.,192.168.; export LD_PRELOAD="/home/$USER/.local/share/jc141/bindToInterface.so"
 export HOME="$R/files/data"; export XDG_DATA_HOME="$R/files/data/.local"; export XDG_CONFIG_HOME="$R/files/data/.config"; mkdir -p {"$HOME","$XDG_CONFIG_HOME","$XDG_DATA_HOME"};
 
