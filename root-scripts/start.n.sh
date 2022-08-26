41e7e7f73c51ed033cac946571699fb501496e15
miocrime <106968863+miocrime@users.noreply.github.com>
2022-08-27 00:39:22 +0300

    Update start.n.sh

--- root-scripts/start.n.sh
+++ root-scripts/start.n.sh
@@ -8,15 +8,14 @@ BINDIR="$R/files/groot"; BIN="game.bin"; CMD=(./"$BIN")
 # gamescope
 : ${GAMESCOPE:=$(command -v gamescope)}; [ -x "$GAMESCOPE" ] && CMD=("$GAMESCOPE" -f -- "${CMD[@]}");
 
-# image handling and exit trap for auto-unmount
-bash "$STS" mount-dwarfs
+# image handling, muting output and exit trap for auto-unmount
+bash "$STS" mount-dwarfs; [ "${DBG:=0}" = "1" ] || exec &>/dev/null
 function cleanup { cd "$OLDPWD" && bash "$STS" unmount-dwarfs; }
 trap 'cleanup' EXIT INT SIGINT SIGTERM
 
 zcat "$LOGO"
 
-# mute output by default, block non-lan networking and redirect home path to isolation
-[ "${DBG:=0}" = "1" ] || exec &>/dev/null
+# block non-lan networking and redirect home path to isolation
 export BIND_INTERFACE=lo; export BIND_EXCLUDE=10.,172.16.,192.168.; export LD_PRELOAD="/home/$USER/.local/share/jc141/bindToInterface.so"
 export HOME="$R/files/data"; export XDG_DATA_HOME="$R/files/data/.local"; export XDG_CONFIG_HOME="$R/files/data/.config"; mkdir -p {"$HOME","$XDG_CONFIG_HOME","$XDG_DATA_HOME"};
 
