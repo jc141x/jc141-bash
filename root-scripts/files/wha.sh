9942a3025356e82645897bb969bfd91d8d5c8ff9
miocrime <106968863+miocrime@users.noreply.github.com>
2022-08-28 12:13:16 +0300

    Update wha.sh

--- root-scripts/files/wha.sh
+++ root-scripts/files/wha.sh
@@ -11,10 +11,4 @@ wine-ge() { [ -x "/bin/wine-ge" ] && echo "detected wine-ge on system" && exit |
 [ ! -f "$PWD/wine-ge.tar.lzma" ] && echo "download failed" && exit || echo "wine-ge.tar.lzma downloaded"
 echo "extracting wine-ge" && tar -xvf "$PWD/wine-ge.tar.lzma" > /dev/null && mv "wine" "$BINDIR/wine"; }
 
-wine-tkg-nomingw() { [ -x "/bin/wine-tkg-nomingw" ] && echo "detected wine-tkg-nomingw on system" && exit || echo "wine-tkg-nomingw not detected locally"
-[ -x "$BINDIR/wine/bin/wine" ]  && echo "wine-tkg-nomingw found on relative path" && exit || echo "wine-tkg-nomingw not found on relative path"
-[ ! -f "$PWD/wine-tkg-nomingw.tar.lzma" ] && echo "wine-tkg-nomingw.tar.lzma not found, downloading" && URL="$(curl -s https://api.github.com/repos/jc141x/wine-tkg-nomingw/releases | awk -F '["]' '/"browser_download_url":/ && /tar.lzma/ {print $4}' | head -n 1)" && curl -L "$URL" -o "wine-tkg-nomingw.tar.lzma"
-[ ! -f "$PWD/wine-tkg-nomingw.tar.lzma" ] && echo "download failed" && exit || echo "wine-tkg-nomingw.tar.lzma downloaded"
-echo "extracting wine-tkg-nomingw" && tar -xvf "$PWD/wine-tkg-nomingw.tar.lzma" > /dev/null && mv "wine" "$BINDIR/wine"; }
-
 for i in "$@"; do if type "$i" &>/dev/null; then "$i"; fi; done
