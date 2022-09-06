16e8146482b7f5023b3ab64ebf1f70e261bf3c94
miocrime <106968863+miocrime@users.noreply.github.com>
2022-09-06 15:35:52 +0300

    Update settings.sh

--- root-scripts/settings.sh
+++ root-scripts/settings.sh
@@ -1,18 +1,13 @@
 #!/bin/bash
 [ ! -x "$(command -v dwarfs)" ] && echo "dwarfs not installed" && exit; [ ! -x "$(command -v fuse-overlayfs)" ] && echo "fuse-overlayfs not installed" && exit
-JCD="${XDG_DATA_HOME:-$HOME/.local/share}/jc141"; [ ! -d "$JCD" ] && mkdir -p "$JCD"; F="$PWD/files"; BASE="$JCD/base"; BASEARCH="$JCD/base.tar.xz";
-PRF="$JCD/prefix-v2.dwarfs"; BASEINSTALL="$JCD/base/install.sh"; GAME="$F/groot.dwarfs"; GRT="$F/groot"; PRFMT="$JCD/prefix-mnt"; GMNT="$F/groot-mnt"
+JCD="${XDG_DATA_HOME:-$HOME/.local/share}/jc141"; [ ! -d "$JCD" ] && mkdir -p "$JCD"; F="$PWD/files"; BASE="$JCD/base";
+PRF="$JCD/prefix-v3.dwarfs"; VLK="$PWD/files/vlk.sh"; GAME="$F/groot.dwarfs"; GRT="$F/groot"; PRFMT="$JCD/prefix-mnt"; GMNT="$F/groot-mnt"
 
 mount-dwarfs() { unmount-game &> /dev/null; [ -d "$GRT" ] && [ "$( ls -A "$GRT")" ] && echo "game is already mounted or extracted" && exit
 mkdir -p {"$GMNT","$F/groot-rw","$F/groot-work","$GRT"} && dwarfs "$GAME" "$GMNT" && fuse-overlayfs -o lowerdir="$GMNT",upperdir="$F/groot-rw",workdir="$F/groot-work" "$GRT" && echo "mounted game"; }
 
-mount-prefix() { unmount-prefix &> /dev/null; BASELNK="$(curl -s https://api.github.com/repos/jc141x/WINDEP/releases/latest)"; DLRLS="$(echo "$BASELNK" | awk -F '["]' '/"browser_download_url":/ && /tar.xz/ {print $4}')"
-[ ! -f "$BASEARCH" ] && [ ! -d "$BASE" ] && curl -L "$DLRLS" -o "$BASEARCH" && [ ! -f "$BASEARCH" ] && echo "download failed" && exit
-
-export WINEPREFIX="$JCD/prefix"; [ -f "$PRF" ] && find "$PRF" -mtime +7 -type f -delete
-[ -f "$BASEARCH" ] && [ ! -f "$PRF" ] && tar -xvf "$BASEARCH" -C "$JCD" > /dev/null
-[ -f "$BASEARCH" ] && [ ! -f "$PRF" ] && WINEPREFIX="$JCD/prefix" bash "$BASEINSTALL" && sleep 2 && mkdwarfs -l5 -i "$JCD/prefix" -o "$PRF" && rm -Rf "$WINEPREFIX" && rm -Rf "$BASE";
-
+mount-prefix() { unmount-prefix &> /dev/null; export WINEPREFIX="$JCD/prefix"; [ -f "$PRF" ] && find "$PRF" -mtime +7 -type f -delete
+[ ! -f "$PRF" ] && WINEPREFIX="$JCD/prefix" && bash "$VLK" dxvk && bash "$VLK" vkd3d && mkdwarfs -l6 -i "$JCD/prefix" -o "$PRF" && rm -Rf "$WINEPREFIX";
 [ ! -d "$WINEPREFIX" ] && mkdir -p {"$PRFMT","$F/data/user-data","$F/data/work","$F/data/prefix-tmp"} && dwarfs "$PRF" "$PRFMT" -o cache_image && fuse-overlayfs -o lowerdir="$PRFMT",upperdir="$F/data/user-data",workdir="$F/data/work" "$F/data/prefix-tmp"; echo "mounted prefix"; }
 
 unmount-dwarfs() { killall gamescope && fuser -k "$GMNT"; fusermount3 -u -z "$GRT"; fusermount3 -u -z "$GMNT" && rm -d -f "$GMNT" && rm -d -f "$F/groot-work"; echo "unmounted dwarfs"; }
