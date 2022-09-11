9912efc125ddcaae8e4b286d5c39a248324657a3
miocrime <106968863+miocrime@users.noreply.github.com>
2022-09-11 04:13:25 +0300

    Update settings.sh

--- root-scripts/settings.sh
+++ root-scripts/settings.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 [ ! -x "$(command -v dwarfs)" ] && echo "dwarfs not installed" && exit; [ ! -x "$(command -v fuse-overlayfs)" ] && echo "fuse-overlayfs not installed" && exit
 JCD="${XDG_DATA_HOME:-$HOME/.local/share}/jc141"; [ ! -d "$JCD" ] && mkdir -p "$JCD"; F="$PWD/files"; BASE="$JCD/base";
-PRF="$JCD/prefix-v3.dwarfs"; GAME="$F/groot.dwarfs"; GRT="$F/groot"; PRFMT="$JCD/prefix-mnt"; GMNT="$F/groot-mnt"; THREADS="2";
+PRF="$JCD/prefix-v3.dwarfs"; GAME="$F/groot.dwarfs"; GRT="$F/groot"; PRFMT="$JCD/prefix-mnt"; GMNT="$F/groot-mnt";
 
 mount-dwarfs() { unmount-game &> /dev/null; [ -d "$GRT" ] && [ "$( ls -A "$GRT")" ] && echo "game is already mounted or extracted" && exit
 mkdir -p {"$GMNT","$F/groot-rw","$F/groot-work","$GRT"} && dwarfs "$GAME" "$GMNT" && fuse-overlayfs -o lowerdir="$GMNT",upperdir="$F/groot-rw",workdir="$F/groot-work" "$GRT" && echo "mounted game"; }
@@ -12,7 +12,7 @@ mount-prefix() { unmount-prefix &> /dev/null; export WINEPREFIX="$JCD/prefix"; [
 
 unmount-dwarfs() { killall gamescope && fuser -k "$GMNT"; fusermount3 -u -z "$GRT"; fusermount3 -u -z "$GMNT" && rm -d -f "$GMNT" && rm -d -f "$F/groot-work"; echo "unmounted dwarfs"; }
 unmount-prefix() { fuser -k "$PRFMT"; fusermount3 -u -z "$F/data/prefix-tmp" && rm -d -f "$F/data/prefix-tmp"; fusermount3 -u -z "$PRFMT" && rm -d -f "$PRFMT"; echo "unmounted prefix"; }
-extract-dwarfs() { [ -d "$GRT" ] && [ "$( ls -A "$GRT")" ] && echo "game is already mounted or extracted" && exit; mkdir "$GRT"; dwarfsextract -n="$THREADS" -i "$GAME" -o "$GRT"; }
+extract-dwarfs() { [ -d "$GRT" ] && [ "$( ls -A "$GRT")" ] && echo "game is already mounted or extracted" && exit; mkdir "$GRT"; dwarfsextract -i "$GAME" -o "$GRT"; }
 delete-dwarfs-image() { rm -Rf "$GAME"; }
 compress-game() { [ ! -f "$GAME" ] && mkdwarfs -l7 -B30 -i "$GRT" -o "$GAME"; }
 
