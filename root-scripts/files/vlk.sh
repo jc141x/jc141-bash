a812e25047d48fb7688d0d76149a52e254b64b2e
miocrime <106968863+miocrime@users.noreply.github.com>
2022-09-12 21:55:46 +0300

    Update vlk.sh

--- root-scripts/files/vlk.sh
+++ root-scripts/files/vlk.sh
@@ -3,9 +3,9 @@ extract() { tar -xvf "$1" &>/dev/null; }; download() { command -v curl >/dev/nul
 SYSDXVK="$(command -v setup_dxvk)"; SYSVKD3D="$(command -v setup_vkd3d_proton)"
 DXVK_URL="$(curl -s https://api.github.com/repos/jc141x/dxvk/releases/latest | awk -F '["]' '/"browser_download_url":/ {print $4}')"; DXVK="$(basename "$DXVK_URL")"
 VKD_URL="$(curl -s https://api.github.com/repos/jc141x/vkd3d-proton/releases/latest | awk -F '["]' '/"browser_download_url":/ {print $4}')"; VKD3D="$(basename "$VKD_URL")";
+locally() { echo "using vkd3d and dxvk from system" && "$SYSVKD3D" install && "$SYSDXVK" install && wineserver -w; }
 
-[ -n "$SYSDXVK" ] && echo "using dxvk from system" && "$SYSDXVK" install && wineserver -w
-[ -z "$SYSDXVK" ] && echo "using external dxvk" && [ ! -f "$DXVK" ] && download "$DXVK_URL"; extract "$DXVK" || { rm "$DXVK" && echo "failed to extract dxvk" && return 1; } && cd "${DXVK//.tar.gz/}" || exit; ./setup_dxvk.sh install && wineserver -w; cd "$OLDPWD" || exit; rm -rf "${DXVK//.tar.gz/}";
+externally() { echo "using external dxvk and vkd3d" && download "$DXVK_URL" && extract "$DXVK" || { rm "$DXVK" && echo "failed to extract dxvk" && return 1; } && cd "${DXVK//.tar.gz/}" || exit && ./setup_dxvk.sh install && wineserver -w && cd "$OLDPWD" || exit && rm -rf "${DXVK//.tar.gz/}"
+download "$VKD_URL" && extract "$VKD3D" || { rm "$VKD3D" && echo "failed to extract vkd3d" && return 1; } && cd "${VKD3D//.tar.zst/}" || exit && ./setup_vkd3d_proton.sh install && wineserver -w && cd "$OLDPWD" || exit && rm -rf "${VKD3D//.tar.zst/}"; }
 
-[ -n "$SYSVKD3D" ] && echo "using vkd3d from system" && "$SYSVKD3D" install && wineserver -w
-[ -z "$SYSVKD3D" ] && echo "using external vkd3d" && [ ! -f "$VKD3D" ] && download "$VKD_URL"; extract "$VKD3D" || { rm "$VKD3D" && echo "failed to extract vkd3d" && return 1; } && cd "${VKD3D//.tar.zst/}" || exit; ./setup_vkd3d_proton.sh install && wineserver -w; cd "$OLDPWD" || exit; rm -rf "${VKD3D//.tar.zst/}";
+[ -f "$SYSVKD3D" ] && [ -f "$SYSDXVK" ] && locally || externally
