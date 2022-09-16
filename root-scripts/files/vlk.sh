33a85377982e6be45a21bc06d6f9c9937b240753
miocrime <106968863+miocrime@users.noreply.github.com>
2022-09-16 17:07:16 +0300

    Update vlk.sh

--- root-scripts/files/vlk.sh
+++ root-scripts/files/vlk.sh
@@ -12,8 +12,8 @@ cd "${DXVK//.tar.gz/}" || exit; ./setup_dxvk.sh install > /dev/null && wineserve
 
 dxvk() { DXVKVER="$(curl -s -m 5 https://api.github.com/repos/jc141x/dxvk/releases/latest | awk -F '["/]' '/"browser_download_url":/ {print $11}' | cut -c 2-)"; SYSDXVK="$(command -v setup_dxvk)"
     dxvk() {
-    [ -n "$SYSDXVK" ] && echo "using local dxvk" && "$SYSDXVK" install --symlink > /dev/null && wineserver -w && applied
-    [ -z "$SYSDXVK" ] && echo "using external dxvk" && github_dxvk && echo "$DXVKVER" >"$WINEPREFIX/.dxvk"; }
+    [ -n "$SYSDXVK" ] && echo "using dxvk from system" && "$SYSDXVK" install --symlink > /dev/null && wineserver -w && applied
+    [ -z "$SYSDXVK" ] && echo "using dxvk from github" && github_dxvk && echo "$DXVKVER" >"$WINEPREFIX/.dxvk"; }
     [[ ! -f "$WINEPREFIX/.dxvk" && -z "$(status)" ]] && dxvk
     [[ -f "$WINEPREFIX/.dxvk" && -n "$DXVKVER" && "$DXVKVER" != "$(awk '{print $1}' "$WINEPREFIX/.dxvk")" ]] && { rm -f dxvk-*.tar.gz || true; } && echo -n "updating dxvk" && dxvk
 echo -n "dxvk up-to-date"; }
@@ -26,8 +26,8 @@ cd "$OLDPWD" || exit; rm -rf "${VKD3D//.tar.zst/}"; }
 vkd3d() { VKD3DVER="$(curl -s -m 5 https://api.github.com/jc141x/vkd3d-proton/releases/latest | awk -F '["/]' '/"browser_download_url":/ {print $11}' | cut -c 2-)"
     SYSVKD3D="$(command -v setup_vkd3d_proton)"
     vkd3d() {
-        [ -n "$SYSVKD3D" ] && echo "using local vkd3d." && "$SYSVKD3D" install --symlink > /dev/null && wineserver -w && applied
-        [ -z "$SYSVKD3D" ] && echo "using external vkd3d." && github_vkd3d && echo "$VKD3DVER" >"$WINEPREFIX/.vkd3d"; }
+        [ -n "$SYSVKD3D" ] && echo "using vkd3d from system" && "$SYSVKD3D" install --symlink > /dev/null && wineserver -w && applied
+        [ -z "$SYSVKD3D" ] && echo "using vkd3d from github" && github_vkd3d && echo "$VKD3DVER" >"$WINEPREFIX/.vkd3d"; }
 [[ ! -f "$WINEPREFIX/.vkd3d" && -z "$(status)" ]] && vkd3d
 [[ -f "$WINEPREFIX/.vkd3d" && -n "$VKD3DVER" && "$VKD3DVER" != "$(awk '{print $1}' "$WINEPREFIX/.vkd3d")" ]] && { rm -f vkd3d-proton-*.tar.zst || true; } && echo "updating vkd3d" && vkd3d
 echo -n "vkd3d up-to-date"; }
