13e5bef5df5355f422e24136de7ad91f313b2846
Mio <106968863+miocrime@users.noreply.github.com>
2022-10-27 15:46:42 +0300

    Update start.n.sh

--- root-scripts/start.n.sh
+++ root-scripts/start.n.sh
@@ -3,20 +3,17 @@
 cd "$(dirname "$(readlink -f "$0")")" || exit; [ "$EUID" = "0" ] && exit; R="$PWD"; STS="$R/settings.sh"; LOGO="$PWD/files/logo.txt.gz"; [ ! -e "$LOGO" ] && cp /opt/jc141/bash/logo.txt.gz "$LOGO";
 export JCDN="${XDG_DATA_HOME:-$HOME/.local/share}/jc141/native"; [ ! -d "$JCDN" ] && mkdir -p "$JCDN"
 
-# path defining
-BINDIR="$R/files/groot"; BIN="game.bin"; CMD=(./"$BIN")
-
-# gamescope
-: ${GAMESCOPE:=$(command -v gamescope)}; [ -x "$GAMESCOPE" ] && CMD=("$GAMESCOPE" -f -- "${CMD[@]}");
-
 # image handling, muting output and exit trap for auto-unmount
 bash "$STS" mount-dwarfs; zcat "$LOGO"; [ "${DBG:=0}" = "1" ] || exec &>/dev/null
 function cleanup { cd "$OLDPWD" && bash "$STS" unmount-dwarfs; }
 trap 'cleanup' EXIT INT SIGINT SIGTERM
 
-# block non-lan networking and redirect home path to isolation
+# path defining
+BINDIR="$R/files/groot"; BIN="game.bin";
+
+# block non-lan networking
 export BIND_INTERFACE=lo; export BIND_EXCLUDE=10.,172.16.,192.168.; export LD_PRELOAD="/usr/lib/bindToInterface.so"
 export HOME="$JCDN"; export XDG_DATA_HOME="$JCDN/local"; export XDG_CONFIG_HOME="$JCDN/config"; mkdir -p {"$HOME","$XDG_CONFIG_HOME","$XDG_DATA_HOME"};
 
 # start
-cd "$BINDIR"; "${CMD[@]}" "$@"
+cd "$BINDIR"; ./"$BIN" "$@"
