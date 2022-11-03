1b765ff2be36ad9467fdb2e20b51d1aa42392779
Mio <106968863+miocrime@users.noreply.github.com>
2022-11-03 23:03:11 +0200

    Update settings.sh

--- root-scripts/settings.sh
+++ root-scripts/settings.sh
@@ -6,6 +6,6 @@ mkdir -p {"$PWD/files/groot-mnt","$PWD/files/groot-rw","$PWD/files/groot-work","
 unmount-dwarfs() { fuser -k "$PWD/files/groot-mnt"; fusermount3 -u -z "$PWD/files/groot"; fusermount3 -u -z "$PWD/files/groot-mnt" && rm -d -f "$PWD/files/groot-mnt" && rm -d -f "$PWD/files/groot-work"; echo "unmounted dwarfs"; }
 extract-dwarfs() { [ -d "$PWD/files/groot" ] && [ "$( ls -A "$PWD/files/groot")" ] && echo "game is already mounted or extracted" && exit; echo "Extracting game, please be patient" && mkdir "$PWD/files/groot"; dwarfsextract -i "$PWD/files/groot.dwarfs" -o "$PWD/files/groot"; }
 delete-dwarfs() { rm -Rf "$PWD/files/groot.dwarfs"; }
-compress-to-dwarfs() { [ ! -f "$PWD/files/groot.dwarfs" ] && mkdwarfs -l7 -B30 -i "$PWD/files/groot" -o "$PWD/files/groot.dwarfs"; }
+compress-to-dwarfs() { [ ! -f "$PWD/files/groot.dwarfs" ] && mkdwarfs -l7 -i "$PWD/files/groot" -o "$PWD/files/groot.dwarfs"; }
 
 for i in "$@"; do if type "$i" &>/dev/null; then "$i"; else exit; fi; done
