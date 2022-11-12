4ce1dfebf1bdc8f2316c7098d1612b51cce88b98
silentnoodlemaster <10667809+silentnoodlemaster@users.noreply.github.com>
2022-11-12 15:44:53 +0200

    Update start.n.sh

--- root-scripts/start.n.sh
+++ root-scripts/start.n.sh
@@ -12,7 +12,7 @@ bash "$STS" mount-dwarfs; zcat "$LOGO"; [ "${DBG:=0}" = "1" ] || exec &>/dev/nul
 function cleanup { cd "$OLDPWD" && bash "$STS" unmount-dwarfs; }; trap 'cleanup' EXIT INT SIGINT SIGTERM
 
 # block non-lan networking
-export BIND_INTERFACE=lo; export BIND_EXCLUDE=10.,172.16.,192.168.; export LD_PRELOAD="/usr/lib/bindToInterface.so"; export LD_PRELOAD="/usr/lib32/bindToInterface.so"
+export BIND_INTERFACE=lo; export BIND_EXCLUDE=10.,172.16.,192.168.; export LD_PRELOAD='/usr/$LIB/bindToInterface.so'
 
 # start
 cd "$PWD/files/groot"; ./"game.bin" "$@"
