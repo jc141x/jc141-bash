565914a5a2d4e5df319b69fdb848f33b82be1946
Mio <106968863+miocrime@users.noreply.github.com>
2022-12-21 22:31:41 +0200

    Update start.e-w.sh

--- root-scripts/start.e-w.sh
+++ root-scripts/start.e-w.sh
@@ -7,10 +7,10 @@ STS="$PWD/settings.sh"; LOGO="$PWD/logo.txt.gz"; export JCDW="${XDG_DATA_HOME:-$
 
 # wine
 export WINE="$(command -v wine)"; 
-export WINEPREFIX="$JCDW/prefix" && echo "Path of the wineprefix (where saves are stored -- except those stored in groot/groot-rw) is: $WINEPREFIX"; export WINEDLLOVERRIDES="mshtml=d;nvapi,nvapi64=n"; export WINE_LARGE_ADDRESS_AWARE=1;
+export WINEPREFIX="$JCDW/prefix"; export WINEDLLOVERRIDES="mshtml=d;nvapi,nvapi64=n"; export WINE_LARGE_ADDRESS_AWARE=1;
 
 # dwarfs
-bash "$STS" mount-dwarfs; zcat "$LOGO"; [ "${DBG:=0}" = "1" ] || { export WINEDEBUG='-all' && exec &>/dev/null; echo "Output muted by default for avoiding performance impact. Unmute with DBG=1."; echo "For any misunderstandings or need of support, join the community on Matrix."; }
+bash "$STS" mount-dwarfs; zcat "$LOGO"; echo "Path of the wineprefix is: $WINEPREFIX (where saves are stored -- except those stored in groot/groot-rw)"; echo "For any misunderstandings or need of support, join the community on Matrix.";
 
 # auto-unmount
 function cleanup { cd "$OLDPWD" && bash "$STS" unmount-dwarfs; }; trap 'cleanup' EXIT INT SIGINT SIGTERM
@@ -32,4 +32,5 @@ else vlk-jc141; fi; export DXVK_ENABLE_NVAPI=1
 export BIND_INTERFACE=lo; export BIND_EXCLUDE=10.,172.16.,192.168.; export LD_PRELOAD='/usr/$LIB/bindToInterface.so';
 
 # start
+[ "${DBG:=0}" = "1" ] || { export WINEDEBUG='-all' && echo "Output muted by default for avoiding performance impact. Unmute with DBG=1." && exec &>/dev/null; }
 cd "$PWD/files/groot"; "$WINE" "game.exe" "$@"
