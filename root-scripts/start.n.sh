bc73db4156dc9ef0a0257e87f0d567cf111a543d
Mio <106968863+miocrime@users.noreply.github.com>
2022-12-21 22:34:18 +0200

    Update start.n.sh

--- root-scripts/start.n.sh
+++ root-scripts/start.n.sh
@@ -3,10 +3,10 @@
 [ ! -x "$(command -v dwarfs)" ] && echo "dwarfs not installed" && exit; [ ! -x "$(command -v fuse-overlayfs)" ] && echo "fuse-overlayfs not installed" && exit; cd "$(dirname "$(readlink -f "$0")")" || exit; [ "$EUID" = "0" ] && exit; 
 
 # define
-STS="$PWD/settings.sh"; LOGO="$PWD/logo.txt.gz"; export JCDN="${XDG_DATA_HOME:-$HOME/.local/share}/jc141/native" && echo "The path for game saves is: $JCDN (don't take it for granted, some games choose to ignore saving in this path)"; [ ! -d "$JCDN" ] && mkdir -p "$JCDN"; export HOME="$JCDN"; export XDG_DATA_HOME="$JCDN/local"; export XDG_CONFIG_HOME="$JCDN/config"; mkdir -p {"$HOME","$XDG_CONFIG_HOME","$XDG_DATA_HOME"};
+STS="$PWD/settings.sh"; LOGO="$PWD/logo.txt.gz"; export JCDN="${XDG_DATA_HOME:-$HOME/.local/share}/jc141/native"; [ ! -d "$JCDN" ] && mkdir -p "$JCDN"; export HOME="$JCDN"; export XDG_DATA_HOME="$JCDN/local"; export XDG_CONFIG_HOME="$JCDN/config"; mkdir -p {"$HOME","$XDG_CONFIG_HOME","$XDG_DATA_HOME"};
 
 # dwarfs
-bash "$STS" mount-dwarfs; zcat "$LOGO"; [ "${DBG:=0}" = "1" ] || { exec &>/dev/null; echo "Output muted by default for avoiding performance impact. Unmute with DBG=1."; echo "For any misunderstandings or need of support, join the community on Matrix."; }
+bash "$STS" mount-dwarfs; zcat "$LOGO"; echo "The path for game saves is: $JCDN (don't take it for granted, some games choose to ignore saving in this path)"; echo "For any misunderstandings or need of support, join the community on Matrix."
 
 # auto-unmount
 function cleanup { cd "$OLDPWD" && bash "$STS" unmount-dwarfs; }; trap 'cleanup' EXIT INT SIGINT SIGTERM
@@ -15,4 +15,5 @@ function cleanup { cd "$OLDPWD" && bash "$STS" unmount-dwarfs; }; trap 'cleanup'
 export BIND_INTERFACE=lo; export BIND_EXCLUDE=10.,172.16.,192.168.; export LD_PRELOAD='/usr/$LIB/bindToInterface.so'
 
 # start
+[ "${DBG:=0}" = "1" ] || { exec &>/dev/null; echo "Output muted by default for avoiding performance impact. Unmute with DBG=1."; }
 cd "$PWD/files/groot"; ./"game.bin" "$@"
