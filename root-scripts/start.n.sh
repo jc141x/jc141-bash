f2da835985f83a82c7e875da1979cf178f9a15c3
adminx01 <106968863+adminx01@users.noreply.github.com>
2023-02-24 14:58:42 +0200

    Update start.n.sh

--- root-scripts/start.n.sh
+++ root-scripts/start.n.sh
@@ -12,7 +12,7 @@ bash "$STS" mount-dwarfs; zcat "$LOGO"; echo "The path for game saves is: $JCDN 
 function cleanup { cd "$OLDPWD" && bash "$STS" unmount-dwarfs; }; trap 'cleanup' EXIT INT SIGINT SIGTERM
 
 # block non-lan networking
-export BIND_INTERFACE=lo; export BIND_EXCLUDE=10.,172.16.,192.168.; export LD_PRELOAD='/usr/$LIB/bindToInterface.so'; [ -f "/usr/lib64/bindToInterface.so" ] && echo "Non-LAN network blocking is enabled." || echo "Non-LAN network blocking is not enabled due to no bindtointerface package."
+export BIND_INTERFACE=lo; export BIND_EXCLUDE=10.,172.16.,192.168.; export LD_PRELOAD='/usr/$LIB/bindToInterface.so'; [ -f "/usr/lib64/bindToInterface.so" ] && echo "WAN blocking is enabled." || echo "WAN blocking is not enabled due to no bindtointerface package."
 
 # start
 [ "${DBG:=0}" = "1" ] || { exec &>/dev/null; echo "Output muted by default for avoiding performance impact. Unmute with DBG=1."; }
