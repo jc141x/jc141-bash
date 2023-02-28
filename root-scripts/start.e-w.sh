7efd81248fb425cf97dde3aa9f420fec1b00a8e9
adminx01 <106968863+adminx01@users.noreply.github.com>
2023-02-28 11:20:01 +0200

    Update start.e-w.sh

--- root-scripts/start.e-w.sh
+++ root-scripts/start.e-w.sh
@@ -8,7 +8,7 @@ export WINE="$(command -v wine)";
 export WINEPREFIX="$JCDW/prefix"; export WINEDLLOVERRIDES="mshtml=d;nvapi,nvapi64=n"; export WINE_LARGE_ADDRESS_AWARE=1;
 
 # dwarfs
-bash "$STS" mount-dwarfs; zcat "$LOGO"; echo "Path of the wineprefix is: $WINEPREFIX"; echo "For any misunderstandings or need of support, join the community on Matrix.";
+bash "$STS" mount-dwarfs; zcat "$LOGO"; echo "Path of the wineprefix is: $WINEPREFIX";
 
 # auto-unmount
 [ "${UNMOUNT:=1}" = "0" ] && echo "Game will not unmount automatically due to user input." || { function cleanup { cd "$OLDPWD" && bash "$STS" unmount-dwarfs; }; trap 'cleanup' EXIT INT SIGINT SIGTERM; echo "Game will unmount automatically once all child processes close. Can be disabled with UNMOUNT=0"; }
@@ -30,5 +30,6 @@ else vlk-jc141; fi; export DXVK_ENABLE_NVAPI=1
 [ "${WANBLK:=1}" = "0" ] && echo "WAN blocking is not enabled due to no bindtointerface package or user input." || { export BIND_INTERFACE=lo; export BIND_EXCLUDE=10.,172.16.,192.168.; export LD_PRELOAD='/usr/$LIB/bindToInterface.so'; [ -f "/usr/lib64/bindToInterface.so" ] && echo "WAN blocking is enabled. Can be disabled with WANBLK=0"; }
 
 # start
+echo "For any misunderstandings or need of support, join the community on Matrix.";
 [ "${DBG:=0}" = "1" ] || { export WINEDEBUG='-all' && echo "Output muted by default to avoid performance impact. Can unmute with DBG=1." && exec &>/dev/null; }
 cd "$PWD/files/groot"; "$WINE" "game.exe" "$@"
