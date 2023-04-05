d1a3b2a79a73e5454649458ef9b3ef7dc85b6757
RoboPatcher24 <108548496+GrootBlouNaai@users.noreply.github.com>
2023-04-05 22:55:16 +0200

    Introduce optional sandboxing
    - Most of main filesystem including but not limited to home directory is now read only
    - Prefix writing to user documents folder is now redirected to $JCD

--- root-scripts/start.e-w.sh
+++ root-scripts/start.e-w.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # checks
 [ ! -x "$(command -v dwarfs)" ] && echo "dwarfs not installed." && exit; [ ! -x "$(command -v fuse-overlayfs)" ] && echo "fuse-overlayfs not installed." && exit; cd "$(dirname "$(readlink -f "$0")")" || exit; [ "$EUID" = "0" ] && exit; STS="$PWD/settings.sh"; LOGO="$PWD/logo.txt.gz";
-export JCD="${XDG_DATA_HOME:-$HOME/.local/share}/jc141"; [ ! -d "$JCD/wine" ] && mkdir -p "$JCD/wine"
+export JCD="${XDG_DATA_HOME:-$HOME/.local/share}/jc141"; [ ! -d "$JCD/wine" ] && mkdir -p "$JCD/wine"; [! -d "$JCD/saves/$GAMENAME" ] && mkdir -p "$JCD/saves/$GAMENAME"
 
 # wine
 export WINE="$(command -v wine)";
@@ -29,7 +29,13 @@ else vlk-jc141; fi; export DXVK_ENABLE_NVAPI=1
 # block WAN
 [ ! -f "/usr/lib64/bindToInterface.so" ] && echo "bindtointerface package not installed, no WAN blocking." || [ "${WANBLK:=1}" = "0" ] && echo "WAN blocking is not enabled due to user input." || { export BIND_INTERFACE=lo; export BIND_EXCLUDE=10.,172.16.,192.168.; export LD_PRELOAD='/usr/$LIB/bindToInterface.so'; echo "bindtointerface WAN blocking enabled."; }
 
+# bubblewrap isolation
+function box_prefix { bwrap --unshare-user --ro-bind / / --bind "$WINEPREFIX/drive_c/users/$USER" "$JCD/saves/$GAMENAME" --dev-bind /dev /dev --bind "$WINEPREFIX" "$WINEPREFIX" --bind "$PWD" "$PWD" --bind /tmp /tmp "$@"; }
+
 # start
 echo "For any misunderstandings or need of support, join the community on Matrix.";
 [ "${DBG:=0}" = "1" ] || { export WINEDEBUG='-all' && echo "Output muted by default to avoid performance impact. Can unmute with DBG=1." && exec &>/dev/null; }
-cd "$PWD/files/groot"; "$WINE" "game.exe" "$@"
+
+cd "$PWD/files/groot"; EXE="game.exe"
+
+[ ! -f "/usr/bin/bwrap" ] || [ "${BWRAP:=1}" = "0" ] && "$WINE" "$EXE" "$@" || box_prefix "$WINE" "$EXE" "$@"
