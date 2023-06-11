3c93c5506819f1a36d6b9b79e88053d9af262c05
adminx01 <106968863+adminx01@users.noreply.github.com>
2023-06-11 18:28:55 +0300

    Update start.n-w.sh

--- root-scripts/start.n-w.sh
+++ root-scripts/start.n-w.sh
@@ -5,8 +5,6 @@
 # wine
 export WINE="$(command -v wine)";
 export WINEPREFIX="$JCD/wine/native-prefix"; export WINEDLLOVERRIDES="winemenubuilder.exe=d;mshtml=d"; export WINE_LARGE_ADDRESS_AWARE=1; export WINE_D3D_CONFIG="renderer=vulkan" && echo "wined3d vulkan renderer is used.";
-"$WINE" reg add "HKEY_CURRENT_USER\Software\Wine\FileOpenAssociations" /v Enable /d N &>/dev/null;
-
 # dwarfs
 bash "$PWD/settings.sh" mount; zcat "$PWD/logo.txt.gz"; echo "Path of the wineprefix is: $WINEPREFIX";
 
